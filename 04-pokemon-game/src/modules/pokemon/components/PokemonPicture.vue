0a351a8a84820ff910521248d0f2c2e08f49cc28
pgarr <pgarrigosgarcia@gmail.com>
2025-02-25 07:10:32 +0100

    Avanzando

--- 04-pokemon-game/src/modules/pokemon/components/PokemonPicture.vue
+++ 04-pokemon-game/src/modules/pokemon/components/PokemonPicture.vue
@@ -2,11 +2,19 @@
   <section>
     <img
       src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/132.svg"
-      alt=""
+      class="brightness-0 h-[200px]"
     />
   </section>
 </template>
 
 <script setup lang="ts"></script>
 
-<style scoped></style>
+<style scoped>
+img {
+  user-select: none;
+  -moz-user-select: none;
+  -ms-user-select: none;
+  -webkit-user-drag: none;
+  -webkit-user-select: none;
+}
+</style>
