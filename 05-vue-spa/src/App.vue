fd0e43ca9192185074812a108271e92ddb8d563b
usuario <pgarrigosgarcia@gmail.com>
2025-03-07 12:48:43 +0100

    Comienzo single page app

--- 05-vue-spa/src/App.vue
+++ 05-vue-spa/src/App.vue
@@ -1,8 +1,22 @@
 <template>
-  <HomePage />
 
+  <nav>
+    <RouterLink to="#/">Home</RouterLink>
+    <RouterLink to="#/features">Features</RouterLink>
+    <RouterLink to="#/pricing">Pricing</RouterLink>
+    <RouterLink to="#/contact">Contact</RouterLink>
+
+  </nav>
+
+
+
+  <main class="bg-slate-100">
+    <RouterView />
+  </main>
 </template>
 
-<script lang="ts" setup>
-  import HomePage from './modules/landing/pages/HomePage.vue';
-</script>
+
+
+
+
+<script lang="ts" setup></script>
