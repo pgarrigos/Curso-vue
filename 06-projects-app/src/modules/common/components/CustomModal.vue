5967b9e2efea0f57b00f9155814bf331061bbcb8
usuario <pgarrigosgarcia@gmail.com>
2025-03-25 07:11:24 +0100

    sigo project-app con Pinia-Gestor de estado

---
+++ 06-projects-app/src/modules/common/components/CustomModal.vue
@@ -0,0 +1,35 @@
+<template>
+  <div>
+    <dialog class="modal" :open="open">
+      <div class="modal-box">
+        <div class="border-b border-blue-500">
+          <!-- Header -->
+          <slot name="header" />
+        </div>
+
+        <div class="my-5">
+          <slot name="body" />
+        </div>
+
+        <div class="pt-2 border-t border-t-blue-500">
+          <slot name="footer" />
+        </div>
+      </div>
+    </dialog>
+
+    <div
+      v-if="open"
+      class="fixed top-0 left-0 z-10 w-screen h-screen bg-black opacity-50 modal-backdrop"
+    ></div>
+  </div>
+</template>
+
+<script lang="ts" setup>
+interface Props {
+  open?: boolean;
+}
+
+withDefaults(defineProps<Props>(), {
+  open: false,
+});
+</script>
--- 06-projects-app/src/modules/projects/views/ProjectsView.vue
+++ 06-projects-app/src/modules/projects/views/ProjectsView.vue
@@ -1,26 +1,28 @@
 <template>
   <div>
-    <div class="w-full overflow-x-auto">
-      <table class="table">
-        <!-- head -->
-        <thead>
-          <tr>
-            <th></th>
-            <th>Projecto</th>
-            <th>Tareas</th>
-            <th>Avance</th>
-          </tr>
-        </thead>
-        <tbody>
-          <tr class="hover">
-            <th>2</th>
-            <td>Hart Hagerty</td>
-            <td>Desktop Support Technician</td>
-            <td>Purple</td>
-          </tr>
-        </tbody>
-      </table>
-    </div>
+    <custom-modal>
+      <div class="w-full overflow-x-auto">
+        <table class="table">
+          <!-- head -->
+          <thead>
+            <tr>
+              <th></th>
+              <th>Projecto</th>
+              <th>Tareas</th>
+              <th>Avance</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr class="hover">
+              <th>2</th>
+              <td>Hart Hagerty</td>
+              <td>Desktop Support Technician</td>
+              <td>Purple</td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
+    </custom-modal>
 
     <InputModal
       :open="modalOpen"
@@ -31,8 +33,32 @@
       sub-title="Pon un nombre único a tu proyecto"
     />
 
+    <custom-modal :open="customModalOpen">
+      <template #header>
+        <h1 class="text-3xl">Titulo del modal</h1>
+      </template>
+
+      <template #body>
+        <p>
+          Officia nulla tempor id voluptate magna proident proident. Adipisicing ipsum do sunt nisi
+          ullamco proident velit ipsum mollit amet fugiat.
+        </p>
+      </template>
+
+      <template #footer>
+        <div class="flex justify-end">
+          <button @click="customModalOpen = false" class="btn mr-4">Close</button>
+          <button @click="customModalOpen = false" class="btn btn-primary">Aceptar</button>
+        </div>
+      </template>
+    </custom-modal>
+
     <fab-button @click="modalOpen = true">
       <AddCircle />
+    </fab-button>
+
+    <fab-button @click="customModalOpen = true" position="bottom-left">
+      <ModalIcon />
     </fab-button>
   </div>
 </template>
@@ -41,9 +67,14 @@
 import InputModal from '../../common/components/InputModal.vue';
 import FabButton from '../../common/components/FabButton.vue';
 import AddCircle from '../../common/icons/AddCircle.vue';
+import CustomModal from '../../common/components/CustomModal.vue';
+import ModalIcon from '../../common/icons/ModalIcon.vue';
 import { ref } from 'vue';
 
 const modalOpen = ref(false);
+const customModalOpen = ref(false);
 
-const onNewValue = (projectName: string) => {};
+const onNewValue = (projectName: string) => {
+  console.log({ projectName });
+};
 </script>
