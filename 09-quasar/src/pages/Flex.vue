1ca57348354aa02b82b481326b322b1aa659c28d
usuario <pgarrigosgarcia@gmail.com>
2025-06-25 06:13:40 +0200

    Terminada sección Quasar

---
+++ 09-quasar/src/pages/Flex.vue
@@ -0,0 +1,126 @@
+<template>
+  <q-page>
+    <div class="row q-mb-md">
+      <div class="col bg-primary"></div>
+      <div class="col bg-secondary"></div>
+      <div class="col bg-accent"></div>
+    </div>
+
+    <div class="row q-mb-md">
+      <!-- 12 slots -->
+      <div class="col bg-primary"></div>
+      <div class="col col-5 bg-secondary"></div>
+      <div class="col col-6 bg-accent"></div>
+    </div>
+
+    <div class="justify-center row q-mb-md">
+      <div class="col col-4 bg-primary"></div>
+      <div class="col col-4 bg-secondary"></div>
+    </div>
+
+    <div class="justify-end row q-mb-md">
+      <div class="col col-4 bg-primary"></div>
+      <div class="col col-4 bg-secondary"></div>
+    </div>
+
+    <div class="justify-between row q-mb-md">
+      <div class="col col-4 bg-primary"></div>
+      <div class="col col-4 bg-secondary"></div>
+    </div>
+
+    <q-separator spaced />
+
+    <div class="justify-between row q-mb-md">
+      <div class="col-12 col-sm-5 col-md-4 bg-primary"></div>
+      <div class="col-12 col-sm-5 col-md-4 bg-secondary"></div>
+    </div>
+
+    <q-separator spaced />
+
+    <div class="row q-mb-md">
+      <div class="square-box bg-primary"></div>
+      <div class="square-box bg-secondary"></div>
+      <div class="square-box bg-accent"></div>
+    </div>
+
+    <div class="justify-between row q-mb-md">
+      <div class="square-box bg-primary"></div>
+      <div class="square-box bg-secondary"></div>
+      <div class="square-box bg-accent"></div>
+    </div>
+
+    <div class="justify-end row q-mb-md">
+      <div class="square-box bg-primary"></div>
+      <div class="square-box bg-secondary"></div>
+      <div class="square-box bg-accent"></div>
+    </div>
+
+    <div class="justify-around row q-mb-md">
+      <div class="square-box bg-primary"></div>
+      <div class="square-box bg-secondary"></div>
+      <div class="square-box bg-accent"></div>
+    </div>
+
+    <div class="items-center column q-mb-md bg-dark">
+      <div class="square-box bg-primary"></div>
+      <div class="square-box bg-secondary"></div>
+      <div class="square-box bg-accent"></div>
+    </div>
+
+    <div class="items-end column q-mb-md bg-dark">
+      <div class="square-box bg-primary"></div>
+      <div class="square-box bg-secondary"></div>
+      <div class="square-box bg-accent"></div>
+    </div>
+
+    <div
+      class="items-center justify-center column custom-height q-mb-md bg-dark"
+    >
+      <div class="square-box bg-primary"></div>
+      <div class="square-box bg-secondary"></div>
+      <div class="square-box bg-accent"></div>
+    </div>
+
+    <div
+      class="items-center justify-between column custom-height q-mb-md bg-dark"
+    >
+      <div class="square-box bg-primary"></div>
+      <div class="square-box bg-secondary"></div>
+      <div class="square-box bg-accent"></div>
+    </div>
+
+    <div
+      class="items-center justify-between column custom-height q-mb-md bg-dark"
+    >
+      <div class="square-box bg-primary"></div>
+      <div class="self-start square-box bg-secondary"></div>
+      <div class="square-box bg-accent"></div>
+    </div>
+  </q-page>
+</template>
+
+<script>
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  name: "Flex",
+});
+</script>
+
+<style scoped>
+.col,
+.col-4,
+.col-6,
+.col-12 {
+  height: 50px;
+}
+
+.square-box {
+  height: 50px;
+  width: 50px;
+}
+
+.custom-height {
+  height: 300px;
+}
+</style>
